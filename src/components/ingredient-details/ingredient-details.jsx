1f621a9ce1c7b42ad8d9061d63002dba1df344d5
Mukhammad <yekut1515@yandex.ru>
2023-09-06 18:13:04 +0300

    fixed: fix bugs propTypes  at the IngredientDetails

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from "./ingredient-details.module.css"
 import PropTypes from "prop-types";
+import { BurgerIngredientsPropTypes } from '../../utils/burger-components-propTypes';
 
 const IngredientDetails = ({ data }) => {
     if (!data) return null;
@@ -25,14 +26,6 @@ const IngredientDetails = ({ data }) => {
 };
 
 
-IngredientDetails.propTypes = {
-    data: PropTypes.shape({
-        image: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        calories: PropTypes.number.isRequired,
-        proteins: PropTypes.number.isRequired,
-        fat: PropTypes.number.isRequired,
-        carbohydrates: PropTypes.number.isRequired,
-    }).isRequired,
-};
+IngredientDetails.propTypes = BurgerIngredientsPropTypes;
+
 export default IngredientDetails;
--- src/utils/burger-components-propTypes.js
+++ src/utils/burger-components-propTypes.js
@@ -6,9 +6,21 @@ const BurgerComponentsPropTypes = {
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-
   })).isRequired,
 };
 
+export const BurgerIngredientsPropTypes = {
+  data: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.shape({
+      image: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      calories: PropTypes.number.isRequired,
+      proteins: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      carbohydrates: PropTypes.number.isRequired,
+    })),
+    PropTypes.object,
+  ]).isRequired,
+};
 
 export default BurgerComponentsPropTypes;
