e034e707e2b30856ec439fee3e52efb993cc722c
Mukhammad <yekut1515@yandex.ru>
2023-10-31 21:14:15 +0300

    fixed: change the backgroud of the  page

--- src/pages/NotFound404/NotFound404.jsx
+++ src/pages/NotFound404/NotFound404.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import rocket from "../../images/NotFound404/rocket.svg"
-import found404 from "../../images/NotFound404/404.svg"
+// import found404 from "../../images/NotFound404/404.svg"
 import earth from "../../images/NotFound404/earth.svg"
 import moon from "../../images/NotFound404/moon.svg"
 import astronaut from "../../images/NotFound404/astronaut.svg"
@@ -9,80 +9,83 @@ import style from "./NotFound404.module.css";
 
 const NotFound404 = () => {
     return (
-        <>
-            <div className={`${style["bg-purple"]}`}>
+        <section className={`${style["bg-purple"]}`}>
 
-                <div className={`${style["stars"]}`}>
-                    <div className={`${style["custom-navbar"]}`}>
-                        <div className={`${style["navbar-links"]}`}>
-                            <ul className={`${style["nav-links-list"]}`}>
-                                <li className={`${style["nav-links"]}`}>
-                                    <Link
-                                        className={`${style["nav-link"]}`}
-                                        to="/"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        Home
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+            <div className={`${style["stars"]}`}>
+                <div className={`${style["custom-navbar"]}`}>
+                    <div className={`${style["navbar-links"]}`}>
+                        <ul className={`${style["nav-links-list"]}`}>
+                            <li className={`${style["nav-links"]}`}>
+                                <Link
+                                    className={`${style["nav-link"]}`}
+                                    to="/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    Домой
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
-                    <div className={`${style["central-body"]}`}>
+                </div>
+                <div className={`${style["central-body"]}`}>
+                    <p className={`${style["page-404-title"]} text text_type_digits-large`}>
+                        404
+                    </p>
+                    <p className={`${style["page-404"]} text text_type_main-large`}>
+                        В этой галактике такая страница не существует!
+                    </p>
+                    {/*<img*/}
+                    {/*    className={`${style["image-404"]}`}*/}
+                    {/*    src={found404}*/}
+                    {/*    alt="Страница которая показывает картинку found404"*/}
+                    {/*/>*/}
+                    <Link
+                        to="/"
+                        className={`${style["btn-go-home"]}`}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        На главную страницу
+                    </Link>
+                </div>
+                <div className={`${style["objects"]}`}>
+                    <img
+                        className={`${style["object_rocket"]}`}
+                        src={rocket}
+                        alt="Картинка из страницы 404 с изображение rocket"
+                    />
+                    <div className={`${style["earth-moon"]}`}>
                         <img
-                            className={`${style["image-404"]}`}
-                            src={found404}
-                            alt="Страница которая показывает картинку found404"
+                            className={`${style["object_earth"]}`}
+                            src={earth}
+                            alt="Картинка земли которая находится в странице 404"
                         />
-                        <Link
-                            to="/"
-                            className={`${style["btn-go-home"]}`}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            GO BACK HOME
-                        </Link>
-                    </div>
-                    <div className={`${style["objects"]}`}>
                         <img
-                            className={`${style["object_rocket"]}`}
-                            src={rocket}
-                            alt="Картинка из страницы 404 с изображение rocket"
+                            className={`${style["object_moon"]}`}
+                            src={moon}
+                            alt="Картинка луны которая находится в странице 404"
                         />
-                        <div className={`${style["earth-moon"]}`}>
-                            <img
-                                className={`${style["object_earth"]}`}
-                                src={earth}
-                                alt="Картинка земли которая находится в странице 404"
-                            />
-                            <img
-                                className={`${style["object_moon"]}`}
-                                src={moon}
-                                alt="Картинка луны которая находится в странице 404"
-                            />
-                        </div>
-                        <div className={`${style["box_astronaut"]}`}>
-                            <img
-                                className={`${style["object_astronaut"]}`}
-                                src={astronaut}
-                                alt="Картинка астронавта который находится в странице 404"
-                            />
-                        </div>
                     </div>
-                    <div className={`${style["glowing_stars"]}`}>
-                        <div className={`${style["star"]}`}></div>
-                        <div className={`${style["star"]}`}></div>
-                        <div className={`${style["star"]}`}></div>
-                        <div className={`${style["star"]}`}></div>
-                        <div className={`${style["star"]}`}></div>
-
+                    <div className={`${style["box_astronaut"]}`}>
+                        <img
+                            className={`${style["object_astronaut"]}`}
+                            src={astronaut}
+                            alt="Картинка астронавта который находится в странице 404"
+                        />
                     </div>
-
+                </div>
+                <div className={`${style["glowing_stars"]}`}>
+                    <div className={`${style["star"]}`}></div>
+                    <div className={`${style["star"]}`}></div>
+                    <div className={`${style["star"]}`}></div>
+                    <div className={`${style["star"]}`}></div>
+                    <div className={`${style["star"]}`}></div>
                 </div>
 
             </div>
-        </>
+
+        </section>
     )
 };
 
